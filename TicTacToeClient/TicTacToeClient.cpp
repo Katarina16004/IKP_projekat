067a8dfbc56808000d240b91076a53b0b6572ed8
Katarina16004 <veljkovickatarina56@gmail.com>
2026-01-06 13:14:45 +0100

    client send connection reqest and recieve initial server response

--- TicTacToeClient/TicTacToeClient.cpp
+++ TicTacToeClient/TicTacToeClient.cpp
@@ -69,11 +69,75 @@ int main()
     cout << "Enter your username: ";
     cin.getline(username, 50);
 
+    // TK: Create and send connection request
+    cout << "\n[TK] Sending connection request..." << endl;
+    ConnectionRequest request(1, 0, username);
+
+    char sendBuffer[DEFAULT_BUFLEN];
+    memset(sendBuffer, 0, DEFAULT_BUFLEN);
+    request.serialize(sendBuffer);
+
+    iResult = send(connectSocket, sendBuffer, DEFAULT_BUFLEN, 0);
+    if (iResult == SOCKET_ERROR) {
+        cerr << "[TK] Send failed: " << WSAGetLastError() << endl;
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
+    cout << "[TK] Connection request sent successfully." << endl;
+
+    // FIRST RESPONSE
+    cout << "\n[TK] Waiting for TOZ response..." << endl;
+    char recvBuffer[DEFAULT_BUFLEN];
+    memset(recvBuffer, 0, DEFAULT_BUFLEN);
+
+    iResult = recv(connectSocket, recvBuffer, DEFAULT_BUFLEN, 0);
+    if (iResult > 0) {
+        ConnectionResponse response;
+        response.deserialize(recvBuffer);
+
+        // Validate checksum
+        if (!response.validateChecksum()) {
+            cerr << "[TK] ERROR: Invalid checksum in response!" << endl;
+            closesocket(connectSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        // Check if accepted or rejected
+        cout << "\n========================================" << endl;
+        if (response.getAccepted()) {
+            cout << "[TK] [OK] CONNECTION ACCEPTED by TOZ" << endl;
+            cout << "Message: " << response.getMessage() << endl;
+            cout << "Tentative Game ID: " << response.getIdGame() << endl;
+        }
+        else {
+            cout << "[TK] [X] CONNECTION REJECTED by TOZ" << endl;
+            cout << "Reason: " << response.getMessage() << endl;
+            cout << "========================================\n" << endl;
+
+            // Connection rejected, close and exit
+            closesocket(connectSocket);
+            WSACleanup();
+            cout << "\nPress Enter to exit... ";
+            cin.get();
+            return 1;
+        }
+        cout << "========================================\n" << endl;
+
+    }
+    else {
+        cerr << "[TK] Failed to receive response from server." << endl;
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
+
     // Cleanup
     closesocket(connectSocket);
     WSACleanup();
 
-    cout << "\nPress Enter to exit...  ";
+    cout << "\nPress Enter to exit... ";
     cin.get();
 
     return 0;
